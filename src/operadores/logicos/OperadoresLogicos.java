e99a8893f4ff24f066e2ca82a63e5f1d2d3ec4b8
henrique <henriqueventurini.xp@gmail.com>
2025-10-21 01:29:11 -0300

    aprendendo operadores

---
+++ src/operadores/logicos/OperadoresLogicos.java
@@ -0,0 +1,13 @@
+package operadores.logicos;
+
+public class OperadoresLogicos {
+    public static void main(String[] args){
+
+        //operadores lógicos
+        //&& (and), || (or), ! (not)
+
+        boolean resultado = true && true;
+        System.out.println(resultado);
+
+    }
+}
---
+++ src/operadores/relacionais/OperadoresRelacionais.java
@@ -0,0 +1,17 @@
+package operadores.relacionais;
+
+public class OperadoresRelacionais {
+
+    public static void main(String[] args){
+
+        //operadores relacionais
+        // == (igual a),    != (difente de), > (maior que), < (menor que)
+        // >= (maior ou igual a), <= (menor ou igual a)
+
+        boolean resultado = 7 > 6;
+
+        System.out.println(resultado);
+
+    }
+
+}
